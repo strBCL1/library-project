0f3ef620b1838ffc0092faa03a48e926771ad5a7
strBCL1 <work.artem.popov@gmail.com>
2022-05-03 11:26:29 +0200

    Modified and tested 'AuthorDTO', 'AuthorMapper' and 'AuthorMapperTest'
    Added set of DTO books to 'AuthorDTO'.
    Implemented default author mapper methods, since they could not map set of domain books to set of DTO books and vice-versa.

--- src/test/java/com/example/libraryproject/api/v1/mapper/AuthorMapperTest.java
+++ src/test/java/com/example/libraryproject/api/v1/mapper/AuthorMapperTest.java
@@ -1,10 +1,15 @@
 package com.example.libraryproject.api.v1.mapper;
 
 import com.example.libraryproject.api.v1.model.AuthorDTO;
+import com.example.libraryproject.api.v1.model.BookDTO;
 import com.example.libraryproject.domain.Author;
+import com.example.libraryproject.domain.Book;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AuthorMapperTest {
 
@@ -21,12 +26,25 @@ class AuthorMapperTest {
         author.setFirstname(FIRSTNAME);
         author.setLastname(LASTNAME);
 
+        Book book1 = new Book();
+        book1.setId(1);
+        book1.setTitle("title1");
+
+        Book book2 = new Book();
+        book2.setId(2);
+        book2.setTitle("title2");
+
+        Set<Book> bookSet = Set.of(book1, book2);
+
+        author.setBooks(bookSet);
+
         AuthorDTO authorDTO = authorMapper.authorToAuthorDto(author);
 
         assertAll("AuthorDTO must have same fields as Author",
                 () -> assertEquals(author.getId(), authorDTO.getId()),
                 () -> assertEquals(author.getFirstname(), authorDTO.getFirstname()),
-                () -> assertEquals(author.getLastname(), authorDTO.getLastname()));
+                () -> assertEquals(author.getLastname(), authorDTO.getLastname()),
+                () -> assertEquals(author.getBooks().size(), authorDTO.getBookDTOSet().size()));
     }
 
     @Test
@@ -36,11 +54,24 @@ class AuthorMapperTest {
         authorDTO.setFirstname(FIRSTNAME);
         authorDTO.setLastname(LASTNAME);
 
+        BookDTO bookDTO1 = new BookDTO();
+        bookDTO1.setId(1);
+        bookDTO1.setTitle("dto_title1");
+
+        BookDTO bookDTO2 = new BookDTO();
+        bookDTO2.setId(2);
+        bookDTO2.setTitle("dto_title2");
+
+        Set<BookDTO> bookDTOSet = Set.of(bookDTO1, bookDTO2);
+
+        authorDTO.setBookDTOSet(bookDTOSet);
+
         Author author = authorMapper.authorDtoToAuthor(authorDTO);
 
         assertAll("Author must have same fields as AuthorDTO",
                 () -> assertEquals(authorDTO.getId(), author.getId()),
                 () -> assertEquals(authorDTO.getFirstname(), author.getFirstname()),
-                () -> assertEquals(authorDTO.getLastname(), author.getLastname()));
+                () -> assertEquals(authorDTO.getLastname(), author.getLastname()),
+                () -> assertEquals(authorDTO.getBookDTOSet().size(), author.getBooks().size()));
     }
 }
