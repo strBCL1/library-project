c934ad4b40898ce3e3f0932e2973ddcd4453e87d
strBCL1 <work.artem.popov@gmail.com>
2022-05-03 20:31:04 +0200

    Implemented and tested 'GET all books' in 'BookController'

--- src/main/java/com/example/libraryproject/controllers/v1/BookController.java
+++ src/main/java/com/example/libraryproject/controllers/v1/BookController.java
@@ -1,7 +1,11 @@
 package com.example.libraryproject.controllers.v1;
 
+import com.example.libraryproject.api.v1.model.BookListDTO;
 import com.example.libraryproject.services.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,11 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public BookListDTO getAllBooks() {
+        return new BookListDTO(bookService.getAllBooks());
     }
 }
---
+++ src/test/java/com/example/libraryproject/controllers/v1/BookControllerTest.java
@@ -0,0 +1,55 @@
+package com.example.libraryproject.controllers.v1;
+
+import com.example.libraryproject.api.v1.model.BookDTO;
+import com.example.libraryproject.services.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class BookControllerTest {
+
+    @Mock
+    BookService bookService;
+
+    @InjectMocks
+    BookController bookController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(bookController)
+                .build();
+    }
+
+    @Test
+    void givenBookDtoList_whenGetAllBooks_thenReturnBookDtoList() throws Exception {
+        List<BookDTO> bookDTOList = Arrays.asList(new BookDTO(1, "title1"), new BookDTO(2, "title2"));
+
+        when(bookService.getAllBooks()).thenReturn(bookDTOList);
+
+        mockMvc.perform(get("/books")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.bookDTOList[0].title", equalTo(bookDTOList.get(0).getTitle())))
+                .andExpect(jsonPath("$.bookDTOList[1].title", equalTo(bookDTOList.get(1).getTitle())));
+    }
+}
