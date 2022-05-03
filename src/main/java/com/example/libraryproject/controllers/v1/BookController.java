a7c171e660e9aa6fb451777c0310ca95b2bba6cc
strBCL1 <work.artem.popov@gmail.com>
2022-05-03 20:14:30 +0200

    Created 'BookController' with 'BookService' dependency injected. Only 'READ' operations allowed since 'Book' does not own the relationship

---
+++ src/main/java/com/example/libraryproject/controllers/v1/BookController.java
@@ -0,0 +1,16 @@
+package com.example.libraryproject.controllers.v1;
+
+import com.example.libraryproject.services.BookService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+}
